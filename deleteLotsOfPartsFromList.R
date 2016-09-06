c8dd94b550a951226c66bb1f3f19388b64dcffd4
IC-iGEM-2016 <igem.imperial2016@gmail.com>
2016-09-06 15:32:33 +0100

    Update deleteLotsOfPartsFromList.R

--- deleteLotsOfPartsFromList.R
+++ deleteLotsOfPartsFromList.R
@@ -1,18 +1,18 @@
 #
-#This file is part of DISC.
+#This file is part of VESICLES.
 
-#    DISC is free software: you can redistribute it and/or modify
+#    VESICLES is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
 #    (at your option) any later version.
 
-#    DISC is distributed in the hope that it will be useful,
+#    VESICLES is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #    GNU General Public License for more details.
 
 #    You should have received a copy of the GNU General Public License
-#    along with DISC.  If not, see <http://www.gnu.org/licenses/>.
+#    along with VESICLES.  If not, see <http://www.gnu.org/licenses/>.
 
 deleteLotsOfPartsFromList<-function(...)
 {
